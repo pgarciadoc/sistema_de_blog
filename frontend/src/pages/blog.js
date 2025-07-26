66cfc3fab9c0231134e5aaaab79793b989e4af62
Pedro Garcia <pgpessoal@outlook.com>
2025-07-25 23:22:47 -0300

    Modularizando página inicial

---
+++ frontend/src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import PostPage from "../PostPage";
+
+function Blog() {
+    return (
+        PostPage()
+    );
+}
+export default Blog
