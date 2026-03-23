17532304c339c4d1299a1b7361c710caf4b5af31
Pedro Garcia <142314975+pgarciadoc@users.noreply.github.com>
2026-03-23 17:14:31 -0300

    Update blog.js

--- frontend/src/pages/blog.js
+++ frontend/src/pages/blog.js
@@ -1,11 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import PostPage from "../PostPage";
+import api from "../services/api";
 
 function Blog() {
-    return (
-        PostPage()
-    );
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    api.get("/posts")
+      .then(res => {
+        console.log(res.data);
+        setPosts(res.data);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1>Artigos do Blog</h1>
+
+      {posts.map((post, i) => (
+        <div key={i} className="card mb-3 p-3">
+          <h2>{post.titulo}</h2>
+          <p><strong>Data:</strong> {post.data}</p>
+
+          <div
+            dangerouslySetInnerHTML={{ __html: post.conteudo }}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
-export default Blog
+
+export default Blog;
